f2449151d5e15288aa2199966cfdd802d21b1b95
defqd <DefQD@yandex.ru>
2022-12-08 21:09:22 +0300

    2236. Root Equals Sum of Children

---
+++ LeetCode/LeetCode.Tests/Easy/2236_RootEqualsSumOfChildrenTests.cs
@@ -0,0 +1,44 @@
+using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class RootEqualsSumOfChildrenTests
+    {
+        [TestCaseSource(typeof(RootEqualsSumOfChildrenTestSource))]
+        public void CheckTreeTest(TreeNode tree, bool expected)
+        {
+            var result = RootEqualsSumOfChildren.CheckTree(tree);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    public class RootEqualsSumOfChildrenTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new TreeNode
+                {
+                    val = 10,
+                    left = new TreeNode { val = 6 },
+                    right = new TreeNode { val = 4 }
+                },
+                true
+            };
+
+            yield return new object[]
+            {
+                new TreeNode
+                {
+                    val = 5,
+                    left = new TreeNode { val = 3 },
+                    right = new TreeNode { val = 1 }
+                },
+                false
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/2236_RootEqualsSumOfChildren.cs
@@ -0,0 +1,37 @@
+﻿/*
+ 
+2236. Root Equals Sum of Children
+
+https://leetcode.com/problems/root-equals-sum-of-children/description/
+
+You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+*/
+
+namespace LeetCode.Easy
+{
+    public static class RootEqualsSumOfChildren
+    {
+        public static bool CheckTree(TreeNode root)
+        {
+            return root.val == root.left.val + root.right.val;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 10, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}

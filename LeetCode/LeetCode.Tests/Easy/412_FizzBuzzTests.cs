b39f7a9282db3643c731f0346bf836edab3d8333
defqd <DefQD@yandex.ru>
2022-12-09 11:22:13 +0300

    412. Fizz Buzz

---
+++ LeetCode/LeetCode.Tests/Easy/412_FizzBuzzTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCaseSource(typeof(FizzBuzzTestSource))]
+        public void FizzBuzzTest(int n, List<string> expected)
+        {
+            var result = _FizzBuzz.FizzBuzz(n);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class FizzBuzzTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { 3, new List<string> { "1", "2", "Fizz" } };
+            yield return new object[] { 5, new List<string> { "1", "2", "Fizz", "4", "Buzz" } };
+            yield return new object[] { 15, new List<string> { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" } };
+        }
+    }
+}

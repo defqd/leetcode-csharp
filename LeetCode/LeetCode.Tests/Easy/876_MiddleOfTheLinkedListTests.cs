aebef3913cf84a4464052fbd10277c9e39115c1a
defqd <DefQD@yandex.ru>
2022-12-10 22:45:30 +0300

    876. Middle of the Linked List

---
+++ LeetCode/LeetCode.Tests/Easy/876_MiddleOfTheLinkedListTests.cs
@@ -0,0 +1,90 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class MiddleOfTheLinkedListTests
+    {
+        [TestCaseSource(typeof(MiddleOfTheLinkedListTestSource))]
+        public void MiddleNodeTest(ListNode head, ListNode expected)
+        {
+            var result = MiddleOfTheLinkedList.MiddleNode(head);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        class MiddleOfTheLinkedListTestSource : IEnumerable
+        {
+            public ListNode Middle(ListNode head, int index)
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    head = head.next;
+                }
+                return head;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                var firstList = new ListNode
+                {
+                    val = 1,
+                    next = new ListNode
+                    {
+                        val = 2,
+                        next = new ListNode
+                        {
+                            val = 3,
+                            next = new ListNode
+                            {
+                                val = 4,
+                                next = new ListNode
+                                {
+                                    val = 5
+                                }
+                            }
+                        }
+                    }
+                };
+
+                var secondList = new ListNode
+                {
+                    val = 1,
+                    next = new ListNode
+                    {
+                        val = 2,
+                        next = new ListNode
+                        {
+                            val = 3,
+                            next = new ListNode
+                            {
+                                val = 4,
+                                next = new ListNode
+                                {
+                                    val = 5,
+                                    next = new ListNode
+                                    {
+                                        val = 6
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+
+                yield return new object[]
+                {
+                    firstList,
+                    Middle(firstList, 2)
+                };
+
+                yield return new object[]
+                {
+                    secondList,
+                    Middle(secondList, 3)
+                };
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/876_MiddleOfTheLinkedList.cs
@@ -0,0 +1,70 @@
+﻿/*
+ 
+876. Middle of the Linked List
+
+https://leetcode.com/problems/middle-of-the-linked-list/description/
+ 
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one. 
+*/
+
+using System.Collections.Generic;
+
+namespace LeetCode.Easy
+{
+    public static class MiddleOfTheLinkedList
+    {
+        public static ListNode MiddleNode(ListNode head)
+        {
+            ListNode middle = head;
+            ListNode end = head;
+
+            while (end != null && end.next != null)
+            {
+                middle = middle.next;
+                end = end.next.next;
+            }
+
+            return middle;
+
+            //var list = new List<ListNode>();
+
+            //int lenght = 0;
+
+            //while (head != null)
+            //{
+            //    list.Add(head);
+            //    head = head.next;
+            //    lenght++;
+            //}
+
+            //return list.ElementAt(lenght / 2);
+        }
+    }
+ 
+    //Definition for singly-linked list.
+    public class ListNode 
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null) 
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}

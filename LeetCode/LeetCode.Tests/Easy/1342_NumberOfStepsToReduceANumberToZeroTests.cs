ba98795b76e7e22c6f40b8dcfa0a0e9b4671c394
defqd <DefQD@yandex.ru>
2022-12-10 20:50:26 +0300

    1342. Number of Steps to Reduce a Number to Zero

---
+++ LeetCode/LeetCode.Tests/Easy/1342_NumberOfStepsToReduceANumberToZeroTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Easy;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class NumberOfStepsToReduceANumberToZeroTests
+    {
+        [TestCase(14, 6)]
+        [TestCase(8, 4)]
+        [TestCase(123, 12)]
+        public void NumberOfStepsTest(int num, int expected)
+        {
+            var result = NumberOfStepsToReduceANumberToZero.NumberOfSteps(num);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}

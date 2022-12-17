29ea58733f6a06da2effb36f3bbc54d78eac87f8
defqd <DefQD@yandex.ru>
2022-12-17 21:28:09 +0300

    485. Max Consecutive Ones

---
+++ LeetCode/LeetCode.Tests/Easy/485_MaxConsecutiveOnesTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class MaxConsecutiveOnesTests
+    {
+        [TestCaseSource(typeof(FindMaxConsecutiveOnesTestSource))]
+        public void FindMaxConsecutiveOnesTest(int[] nums, int expected)
+        {
+            var result = MaxConsecutiveOnes.FindMaxConsecutiveOnes(nums);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        class FindMaxConsecutiveOnesTestSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { new int[] { 1, 1, 0, 1, 1, 1 }, 3 };
+                yield return new object[] { new int[] { 1, 0, 1, 1, 0, 1 }, 2 };
+                yield return new object[] { new int[] { 1, 0, 0, 0, 0, 0 }, 1 };
+                yield return new object[] { new int[] { 0, 0, 0, 0, 0, 1 }, 1 };
+                yield return new object[] { new int[] { 0, 0, 0, 0, 0, 0 }, 0 };
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/485_MaxConsecutiveOnes.cs
@@ -0,0 +1,57 @@
+﻿/*
+
+485. Max Consecutive Ones
+
+https://leetcode.com/problems/max-consecutive-ones/description/
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ 
+*/
+
+namespace LeetCode.Easy
+{
+    public static class MaxConsecutiveOnes
+    {
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int count = 0;
+            int max = 0;
+
+            foreach (int n in nums)
+            {
+                if (n == 1)
+                    count++;
+                else
+                    count = 0;
+
+                if (count > max)
+                    max = count;
+            }
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] == 1)
+            //    {
+            //        count++;
+            //    }
+            //    else
+            //        count = 0;
+
+            //    if (count > max)
+            //        max = count;
+            //}
+
+            return max;
+        }
+    }
+}

f1decf73f18f21fbdb64628cfcf542d771d36a0f
defqd <DefQD@yandex.ru>
2022-12-18 19:41:44 +0300

    1295. Find Numbers with Even Number of Digits

---
+++ LeetCode/LeetCode.Tests/Easy/1295_FindNumbersWithEvenNumberOfDigitsTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class FindNumbersWithEvenNumberOfDigitsTests
+    {
+        [TestCaseSource(typeof(FindNumbersWithEvenNumberOfDigitsTestSource))]
+        public void FindNumbersTest(int[] nums, int expected)
+        {
+            var result = FindNumbersWithEvenNumberOfDigits.FindNumbers(nums);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+
+    class FindNumbersWithEvenNumberOfDigitsTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { new int[] { 12, 345, 2, 6, 7896 }, 2 };
+            yield return new object[] { new int[] { 555, 901, 482, 1771 }, 1 };
+            yield return new object[] { new int[] { 437, 315, 322, 431, 686, 264, 442 }, 0 };
+        }
+    }
+}

960163e4e074bb7774bb009009921d423eadc31b
defqd <DefQD@yandex.ru>
2022-12-19 20:00:18 +0300

    977. Squares of a Sorted Array

---
+++ LeetCode/LeetCode.Tests/Easy/977_SquaresOfASortedArrayTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class SquaresOfASortedArrayTests
+    {
+        [TestCaseSource(typeof(SquaresOfASortedArrayTestSource))]
+        public void SortedSquaresTest(int[] nums, int[] expected)
+        {
+            var result = SquaresOfASortedArray.SortedSquares(nums);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class SquaresOfASortedArrayTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { new int[] { -4, -1, 0, 3, 10 }, new int[] { 0, 1, 9, 16, 100 } };
+            yield return new object[] { new int[] { -7, -3, 2, 3, 11 }, new int[] { 4, 9, 9, 49, 121 } };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/977_SquaresOfASortedArray.cs
@@ -0,0 +1,53 @@
+﻿/*
+
+977. Squares of a Sorted Array
+
+https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+*/
+
+namespace LeetCode.Easy
+{
+    public static class SquaresOfASortedArray
+    {
+        public static int[] SortedSquares(int[] nums)
+        {
+            int [] arr = new int[nums.Length];
+            int begIndex = 0;
+            int endIndex = nums.Length - 1;
+            int end = endIndex;
+
+            while (begIndex <= endIndex)
+            {
+                if (Math.Abs(nums[endIndex]) > Math.Abs(nums[begIndex]))
+                {
+                    arr[end--] = nums[endIndex] * nums[endIndex--];
+                }
+                else
+                    arr[end--] = nums[begIndex] * nums[begIndex++];
+            }
+
+            return arr;
+
+            //for (int i = 0; i < nums.Length; i++)
+            //    nums[i] *= nums[i];
+
+            //Array.Sort(nums);
+
+            //return nums;
+        }
+    }
+}

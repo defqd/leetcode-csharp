ec497949475bcac347cb89702aae3a19a52339f0
defqd <DefQD@yandex.ru>
2022-12-20 20:45:10 +0300

    1089. Duplicate Zeros

---
+++ LeetCode/LeetCode.Tests/Easy/1089_DuplicateZerosTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class DuplicateZerosTests
+    {
+        [TestCaseSource(typeof(DuplicateZerosTestSource))]
+        public void DuplicateZerosTest(int[] arr, int[] expected)
+        {
+            var result = DuplicateZeros.DuplicateZeross(arr);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        class DuplicateZerosTestSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { new int [] { 1, 0, 2, 3, 0, 4, 5, 0 }, new int [] { 1, 0, 0, 2, 3, 0, 0, 4 } };
+                yield return new object[] { new int [] { 1, 2, 3 }, new int [] { 1, 2, 3 } };
+                yield return new object[] { new int [] { 0, 0, 1, 2, 3, 4 }, new int [] { 0, 0, 0, 0, 1, 2 } };
+            }
+        }
+    }
+}

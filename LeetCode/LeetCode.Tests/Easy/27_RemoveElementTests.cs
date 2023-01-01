f67e579b411f125e6b4b4d68891786632c96d25b
defqd <DefQD@yandex.ru>
2023-01-02 01:02:44 +0300

    27. Remove Element

---
+++ LeetCode/LeetCode.Tests/Easy/27_RemoveElementTests.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class RemoveElementTests
+    {
+        [TestCaseSource(typeof(RemoveElementTestSource))]
+        public void RemoveElem(int[] nums, int val, int expected)
+        {
+            var result = RemoveElement.RemoveElem(nums, val);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        class RemoveElementTestSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] 
+                { 
+                    new int[] { 3, 2, 2, 3 }, 
+                    3,
+                    2
+                };
+                yield return new object[]
+                {
+                    new int[] { 0,1,2,2,3,0,4,2 },
+                    2,
+                    5
+                };
+                yield return new object[]
+                {
+                    new int[] { 3, 2, 2, 0 },
+                    3,
+                    3
+                };
+            }
+        }
+    }
+}

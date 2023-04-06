3e620782f2fd194a19c1686c79a04c6b151e2ddd
defqd <DefQD@yandex.ru>
2023-04-06 17:21:50 +0300

    1299. Replace Elements with Greatest Element on Right Side

---
+++ LeetCode/LeetCode.Tests/Easy/1299_ReplaceElementsWithGreatestElementOnRightSideTests.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class ReplaceElementsWithGreatestElementOnRightSideTests
+    {
+        [TestCaseSource(typeof(ReplaceElementsWithGreatestElementOnRightSideTestSource))]
+        public void ReplaceElementsTest(int[] arr, int[] expected)
+        {
+            var result = ReplaceElementsWithGreatestElementOnRightSide.ReplaceElements(arr);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class ReplaceElementsWithGreatestElementOnRightSideTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int [] { 17, 18, 5, 4, 6, 1 },
+                new int [] { 18, 6, 6, 6, 1, -1 }
+            };
+
+            yield return new object[]
+            {
+                new int [] { 1, 2, 3, 4, 5, 6 },
+                new int [] { 6, 6, 6, 6, 6, -1 }
+            };
+
+            yield return new object[]
+            {
+                new int [] { 1, 1, 1, 1, 1, 1 },
+                new int [] { 1, 1, 1, 1, 1, -1 }
+            }; 
+
+            yield return new object[]
+            {
+                new int [] { 20 },
+                new int [] { -1 }
+            };
+        }
+    }
+}

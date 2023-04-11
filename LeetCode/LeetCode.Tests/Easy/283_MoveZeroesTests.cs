2bf79f11a944bab23a91cf6f2c176ea06de4b6a8
defqd <DefQD@yandex.ru>
2023-04-11 22:04:08 +0300

    283. Move Zeroes

---
+++ LeetCode/LeetCode.Tests/Easy/283_MoveZeroesTests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class MoveZeroesTests
+    {
+        [TestCaseSource(typeof(MoveZeroesTestSource))]
+        public void MoveZero(int[] nums, int[] expected)
+        {
+            MoveZeroes.MoveZero(nums);
+
+            Assert.That(expected, Is.EqualTo(nums));
+        }
+    }
+
+    class MoveZeroesTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { 0, 1, 0, 3, 12 },
+                new int [] { 1, 3, 12, 0, 0 }
+            };
+            yield return new object[]
+            {
+                new int[] { 0 },
+                new int [] { 0 }
+            };
+            yield return new object[]
+            {
+                new int[] { 1, 0, 1 },
+                new int [] { 1, 1, 0 }
+            };
+        }
+    }
+}

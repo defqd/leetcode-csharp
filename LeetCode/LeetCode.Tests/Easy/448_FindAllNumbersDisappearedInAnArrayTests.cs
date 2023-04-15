88e1c6535a23d7c39725460dd8799b0aa8b6e866
defqd <DefQD@yandex.ru>
2023-04-15 22:10:55 +0300

    448. Find All Numbers Disappeared in an Array

---
+++ LeetCode/LeetCode.Tests/Easy/448_FindAllNumbersDisappearedInAnArrayTests.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class FindAllNumbersDisappearedInAnArrayTests
+    {
+        [TestCaseSource(typeof(FindAllNumbersDisappearedInAnArrayTestSource))]
+        public void FindDisappearedNumbersTest(int[] nums, List<int> expected)
+        {
+            var result = FindAllNumbersDisappearedInAnArray.FindDisappearedNumbers(nums);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class FindAllNumbersDisappearedInAnArrayTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { 4, 3, 2, 7, 8, 2, 3, 1 },
+                new List<int> { 5, 6}
+            };
+            yield return new object[]
+            {
+                new int[] { 1, 1 },
+                new List<int> { 2 }
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/448_FindAllNumbersDisappearedInAnArray.cs
@@ -0,0 +1,80 @@
+﻿/*
+
+448. Find All Numbers Disappeared in an Array
+
+https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+ 
+
+Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+ 
+*/
+
+namespace LeetCode.Easy
+{
+    public static class FindAllNumbersDisappearedInAnArray
+    {
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            var result = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                if (nums[index] > 0)
+                    nums[index] = -nums[index];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] > 0)
+                    result.Add(i + 1);
+
+            return result;
+
+
+
+            //var result = new List<int>();
+
+            //for (int i = 1; i <= nums.Length; i++)
+            //{
+            //    if (!(nums.Contains(i)))
+            //        result.Add(i);
+            //}
+
+            //return result;
+
+
+
+            //var result = new List<int>();
+
+            //result.AddRange(Enumerable.Range(1, nums.Length));
+
+            //foreach (int item in nums)
+            //{
+            //    if (result.Contains(item))
+            //        result.Remove(item);
+            //}
+
+            //return result;
+        }
+    }
+}

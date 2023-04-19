a8c079ae7f1fe8dcb228a6233152fae4536ba9fc
defqd <DefQD@yandex.ru>
2023-04-19 22:04:39 +0300

    392. Is Subsequence

---
+++ LeetCode/LeetCode.Tests/Easy/392_IsSubsequenceTests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class IsSubsequenceTests
+    {
+        [TestCaseSource(typeof(IsSubsequenceTestSource))]
+        public void IsSubsequence(string s, string t, bool expected)
+        {
+            var result = IsSubsequenceSoulution.IsSubsequence(s, t);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class IsSubsequenceTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                "abc",
+                "ahbgdc",
+                true
+            };
+            yield return new object[]
+            {
+                "axc",
+                "ahbgdc",
+                false
+            };
+        }
+    }
+}

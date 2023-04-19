2122f49c9ef3995489f9e1ab674493b1387459a6
defqd <DefQD@yandex.ru>
2023-04-19 21:03:59 +0300

    1929. Concatenation of Array

---
+++ LeetCode/LeetCode.Tests/Easy/1929_ConcatenationOfArrayTests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class ConcatenationOfArrayTests
+    {
+        [TestCaseSource(typeof(ConcatenationOfArrayTestSource))]
+        public void GetConcatenation(int[] nums, int[] expected)
+        {
+            var result = ConcatenationOfArray.GetConcatenation(nums);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class ConcatenationOfArrayTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { 1, 2, 1 },
+                new int[] { 1, 2, 1, 1, 2, 1 }
+            };
+
+            yield return new object[]
+            {
+                new int[] { 1, 3, 2, 1 },
+                new int[] { 1, 3, 2, 1, 1, 3, 2, 1 }
+            };
+        }
+    }
+}

162ee9e62705e9764f260b2cb7a6ae3c86554997
defqd <DefQD@yandex.ru>
2023-04-19 22:37:26 +0300

    58. Length of Last Word

---
+++ LeetCode/LeetCode.Tests/Easy/58_LengthOfLastWordTests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class LengthOfLastWordTests
+    {
+        [TestCaseSource(typeof(LengthOfLastWordTesSource))]
+        public void LengthOfLastWord(string s, int expected) 
+        {
+            var result = LengthOfLastWordSolution.LengthOfLastWord(s);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class LengthOfLastWordTesSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+            "Hello World",
+            5
+            };
+            yield return new object[]
+            {
+            "   fly me   to   the moon  ",
+            4
+            };
+            yield return new object[]
+            {
+            "luffy is still joyboy",
+            6
+            };
+        }
+    }
+}

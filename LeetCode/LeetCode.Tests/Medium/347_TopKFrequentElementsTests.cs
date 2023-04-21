c57e822d08d68eb07f4dc5458d711acc92b99160
defqd <DefQD@yandex.ru>
2023-04-21 19:35:27 +0300

    347. Top K Frequent Elements

---
+++ LeetCode/LeetCode.Tests/Medium/347_TopKFrequentElementsTests.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Medium;
+using System.Collections;
+
+namespace LeetCode.Tests.Medium
+{
+    [TestFixture]
+    public class TopKFrequentElementsTests
+    {
+        [TestCaseSource(typeof(TopKFrequentElementsTestSource))]
+        public void TopKFrequentTest(int[] nums, int k, int[] expected)
+        {
+            var result = TopKFrequentElements.TopKFrequent(nums, k);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class TopKFrequentElementsTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { 1, 1, 1, 2, 2, 3 },
+                2,
+                new int[] { 1, 2 }
+            };
+            yield return new object[]
+            {
+                new int[] { 1 },
+                1,
+                new int[] { 1 }
+            };            
+            yield return new object[]
+            {
+                new int[] { 1, 2 },
+                2,
+                new int[] { 1, 2 }
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Medium/347_TopKFrequentElements.cs
@@ -0,0 +1,51 @@
+﻿/*
+
+347. Top K Frequent Elements
+
+https://leetcode.com/problems/top-k-frequent-elements/
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ 
+*/
+
+namespace LeetCode.Medium
+{
+    public static class TopKFrequentElements
+    {
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            var dic = new Dictionary<int, int>();
+            int[] result = new int[k];
+
+            for (int i = 0; i < nums.Length; i++)
+                dic[nums[i]] = (dic.ContainsKey(nums[i]) ? dic[nums[i]] : 0) + 1;
+
+            var sorted = dic.OrderByDescending(x => x.Value).ToList();
+
+            for (int i = 0; i < k; i++)
+                result[i] = sorted[i].Key;
+
+            return result;
+        }
+    }
+}

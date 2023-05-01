1bceb5abef59911b26a3074fe07bd43a4e471691
defqd <DefQD@yandex.ru>
2023-05-01 20:24:29 +0300

    14. Longest Common Prefix

---
+++ LeetCode/LeetCode.Tests/Easy/14_LongestCommonPrefixTests.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class LongestCommonPrefixTests
+    {
+        [TestCaseSource(typeof(LongestCommonPrefixTestSource))]
+        public void LongestCommonPrefix(string[] strs, string expected)
+        {
+            var result = LongestCommonPrefixSolution.LongestCommonPrefix(strs);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class LongestCommonPrefixTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new string[] { "flower", "flow", "flight" },
+                "fl"
+            };
+            yield return new object[]
+            {
+                new string[] { "reflower", "flow", "flight" },
+                ""
+            };
+            yield return new object[]
+            {
+                new string[] { "dog","racecar","car" },
+                ""
+            };
+            yield return new object[]
+            {
+                new string[] { "flower", "dog", "flow", "flight" },
+                ""
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/14_LongestCommonPrefix.cs
@@ -0,0 +1,55 @@
+﻿/*
+
+14. Longest Common Prefix
+
+https://leetcode.com/problems/longest-common-prefix/
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+ 
+*/
+namespace LeetCode.Easy
+{
+    public static class LongestCommonPrefixSolution
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return "";
+
+            string prefix = strs[0];
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix[..^1]; //prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (prefix == "")
+                        return "";
+                }
+            }
+
+            return prefix;
+        }
+    }
+}

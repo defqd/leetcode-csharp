5b14ba43c37b537cbb22ca016ac8d7578b5836a0
defqd <DefQD@yandex.ru>
2023-05-03 19:02:08 +0300

    118. Pascal's Triangle

---
+++ LeetCode/LeetCode.Tests/Easy/118_PascalTriangleTests.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class PascalTriangleTests
+    {
+        [TestCaseSource(typeof(PascalTriangleTestSource))]
+        public void GenerateTest(int numRows, IList<IList<int>> expected)
+        {
+            var result = PascalTriangle.Generate(numRows);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class PascalTriangleTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                5,
+                new List<IList<int>>
+                {
+                    new List<int> { 1 },
+                    new List<int> { 1, 1 },
+                    new List<int> { 1, 2, 1 },
+                    new List<int> { 1, 3, 3, 1 },
+                    new List<int> { 1, 4, 6, 4, 1 },
+                }
+            };
+            yield return new object[]
+            {
+                1,
+                new List<IList<int>>
+                {
+                    new List<int> { 1 }
+                }
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/118_PascalTriangle.cs
@@ -0,0 +1,95 @@
+﻿/*
+ 
+118. Pascal's Triangle
+
+https://leetcode.com/problems/pascals-triangle/
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= numRows <= 30
+ 
+*/
+
+using System.Collections.Generic;
+
+namespace LeetCode.Easy
+{
+    public static class PascalTriangle
+    {
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            int[][] triangle = new int[numRows][];
+
+            if (numRows == 0)
+                return null;
+
+            for (int i = 0; i < numRows; i++)
+            {
+                triangle[i] = new int[i + 1];
+                triangle[i][0] = triangle[i][i] = 1;
+
+                for (int j = 1; j < i; j++)
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+
+            }
+
+            return triangle;
+
+            //var result = new List<IList<int>>();
+
+            //for (int i = 0; i < numRows; i++)
+            //{
+            //    var temp = new List<int>();
+
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        if (j == 0 || j == i)
+            //            temp.Add(1);
+            //        else
+            //            temp.Add(result[i - 1][j - 1] + result[i - 1][j]);
+            //    }
+
+
+            //    temp.Add(1);
+            //    result.Add(temp);
+            //}
+
+            //return result;
+
+            //var result = new List<IList<int>>
+            //{
+            //    new List<int> { 1 }
+            //};
+
+            //for (int i = 1; i < numRows; i++)
+            //{
+            //    var temp = new List<int> { 1 };
+
+            //    for (int j = 1; j < i; j++)
+            //        temp.Add(result[i - 1][j - 1] + result[i - 1][j]);
+
+            //    temp.Add(1);
+            //    result.Add(temp);
+            //}
+
+            //return result;
+        }
+    }
+}

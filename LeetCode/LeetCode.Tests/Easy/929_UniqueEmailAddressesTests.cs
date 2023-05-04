43b9848d23435be35ca0f90339e95b380fb53e2e
defqd <DefQD@yandex.ru>
2023-05-04 20:50:23 +0300

    929. Unique Email Addresses

---
+++ LeetCode/LeetCode.Tests/Easy/929_UniqueEmailAddressesTests.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class UniqueEmailAddressesTests
+    {
+        [TestCaseSource(typeof(UniqueEmailAddressesTestSource))]
+        public void NumUniqueEmails(string[] emails, int expected)
+        {
+            var result = UniqueEmailAddresses.NumUniqueEmails(emails);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class UniqueEmailAddressesTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new string[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" },
+                2
+            };
+            yield return new object[]
+            {
+                new string[] { "a@leetcode.com", "b@leetcode.com", "c@leetcode.com" },
+                3
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/929_UniqueEmailAddresses.cs
@@ -0,0 +1,98 @@
+﻿/*
+
+929. Unique Email Addresses
+
+https://leetcode.com/problems/unique-email-addresses/
+
+Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+
+For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
+If you add periods '.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+
+For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
+If you add a plus '+' in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered. Note that this rule does not apply to domain names.
+
+For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+It is possible to use both of these rules at the same time.
+
+Given an array of strings emails where we send one email to each emails[i], return the number of different addresses that actually receive mails.
+
+ 
+
+Example 1:
+
+Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+Output: 2
+Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
+Example 2:
+
+Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+Output: 3
+ 
+
+Constraints:
+
+1 <= emails.length <= 100
+1 <= emails[i].length <= 100
+emails[i] consist of lowercase English letters, '+', '.' and '@'.
+Each emails[i] contains exactly one '@' character.
+All local and domain names are non-empty.
+Local names do not start with a '+' character.
+Domain names end with the ".com" suffix.
+ 
+*/
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public static class UniqueEmailAddresses
+    {
+        public static int NumUniqueEmails(string[] emails)
+        {
+            var result = new HashSet<string>();
+
+            for (int i = 0; i < emails.Length; i++)
+            {
+                StringBuilder builder = new();
+
+                string email = emails[i];
+                int len = email.IndexOf('@');
+                for (int j = 0; j < len; j++)
+                {
+                    if (email[j].Equals('.'))
+                        continue;
+
+                    if (email[j].Equals('+'))
+                        break;
+
+                    builder.Append(email[j]);
+                }
+
+                builder.Append(email.AsSpan(len)); //builder.Append(email.Substring(len));
+
+                if (!result.Contains(builder.ToString()))
+                    result.Add(builder.ToString());
+            }
+
+            return result.Count;
+
+            //var result = new HashSet<string>();
+
+            //for (int i = 0; i < emails.Length; i++)
+            //{
+            //    int email = emails[i].IndexOf('@');
+            //    string local = emails[i][..email]; //string local = emails[i].Substring(0, email);
+            //    string domain = emails[i][emails[i].IndexOf('@')..]; //string domain = emails[i].Substring(emails[i].IndexOf('@'));
+
+            //    if (local.Contains('+'))
+            //        local = local[..local.IndexOf('+')]; //local = local.Substring(0, local.IndexOf('+'));
+
+            //    local = local.Replace(".", "");
+
+            //    result.Add(local + domain);
+            //}
+
+            //return result.Count;
+        }
+    }
+}

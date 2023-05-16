a01f739c2a54cff2a550f5ad151bf9565e36be16
defqd <DefQD@yandex.ru>
2023-05-16 22:08:07 +0300

    724. Find Pivot Index

---
+++ LeetCode/LeetCode.Tests/Easy/724_FindPivotIndexTests.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class FindPivotIndexTests
+    {
+        [TestCaseSource(typeof(FindPivotIndexTestSource))]
+        public void PivotIndex(int[] nums, int expected) 
+        {
+            var result = FindPivotIndex.PivotIndex(nums);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class FindPivotIndexTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] 
+            {
+                new int[] { 1, 7, 3, 6, 5, 6 },
+                3
+            };
+            yield return new object[]
+            {
+                new int[] { 1, 2, 3 },
+                -1
+            };
+            yield return new object[]
+            {
+                new int[] { 2, 1, -1 },
+                0
+            };
+
+        }
+    }
+}

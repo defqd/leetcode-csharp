e49ba8e896e6de7fda5d820232e8aabf6429294f
defqd <DefQD@yandex.ru>
2024-02-06 05:02:32 +0300

    2011. Final Value of Variable After Performing Operations

---
+++ LeetCode/LeetCode.Tests/Easy/2011_FinalValueOfVariableAfterPerformingOperationsTests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    [TestFixture]
+    public class FinalValueOfVariableAfterPerformingOperationsTests
+    {
+        [TestCaseSource(typeof(FinalValueOfVariableAfterPerformingOperationsTestSource))]
+        public void FinalValueAfterOperationsTest(string[] operations, int expected)
+        {
+            var result = FinalValueOfVariableAfterPerformingOperations.FinalValueAfterOperations(operations);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class FinalValueOfVariableAfterPerformingOperationsTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new string[] { "--X", "X++", "X++" },
+                1
+            };
+            yield return new object[]
+            {
+                new string[] { "++X", "++X", "X++" },
+                3
+            };
+            yield return new object[]
+            {
+                new string[] { "X++", "++X", "--X", "X--" },
+                0
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/2011_FinalValueOfVariableAfterPerformingOperations.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Easy
+{
+    public class FinalValueOfVariableAfterPerformingOperations
+    {
+        public static int FinalValueAfterOperations(string[] operations)
+        {
+            int x = 0;
+
+            foreach (var operation in operations)
+            {
+                switch (operation)
+                {
+                    case "--X":
+                        --x;
+                        break;
+                    case "X--":
+                        x--;
+                        break;
+                    case "++X":
+                        ++x;
+                        break;
+                    case "X++":
+                        x++;
+                        break;
+                }
+            }
+
+            return x;
+        }
+    }
+}

6be9f30928aba7feb3771ff1380dee02ad05e175
defqd <DefQD@yandex.ru>
2024-02-10 03:58:36 +0300

    1470. Shuffle the Array

---
+++ LeetCode/LeetCode.Tests/Easy/1470_ShuffleTheArrayTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class ShuffleTheArrayTests
+    {
+        [TestCaseSource(typeof(ShuffleTheArrayTestSource))]
+        public void ShuffleTest(int[] array, int n, int[] expected)
+        {
+            var result = ShuffleTheArray.Shuffle(array, n);
+
+            Assert.That(expected, Is.EqualTo(result));
+
+        }
+        class ShuffleTheArrayTestSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new int[] { 2, 5, 1, 3, 4, 7 },
+                    3,
+                    new int[] { 2, 3, 5, 4, 1, 7 }
+                };
+                yield return new object[]
+                {
+                    new int[] { 1, 2, 3, 4, 4, 3, 2, 1 },
+                    4,
+                    new int[] { 1, 4, 2, 3, 3, 2, 4, 1 }
+                };
+                yield return new object[]
+                {
+                    new int[] { 1, 1, 2, 2 },
+                    2,
+                    new int[] { 1, 2, 1, 2 }
+                };
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/1470_ShuffleTheArray.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Easy
+{
+    public static class ShuffleTheArray
+    {
+        public static int[] Shuffle(int[] nums, int n)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < nums.Length / 2; i++)
+            {
+                list.Add(nums[i]);
+                list.Add(nums[i + n]);
+            }
+
+            return list.ToArray();
+
+            //int[] shuffledNums = new int[n * 2];
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    shuffledNums[2 * i] = nums[i];
+            //    shuffledNums[2 * i + 1] = nums[i + n];
+            //}
+
+            //return shuffledNums;
+        }
+    }
+}

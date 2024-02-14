4d1f6ec5aceb96187f65b207d909a753617963b9
defqd <DefQD@yandex.ru>
2024-02-14 03:30:34 +0300

    2942. Find Words Containing Character

---
+++ LeetCode/LeetCode.Tests/Easy/2942_FindWordsContainingCharacterTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class FindWordsContainingCharacterTests
+    {
+        [TestCaseSourceAttribute(typeof(FindWordsContainingCharacterTestSource))]
+        public void FindWordsContainingTest(string[] words, char x, List<int> expected)
+        {
+            var result = FindWordsContainingCharacter.FindWordsContaining(words, x);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class FindWordsContainingCharacterTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new string[] { "leet", "code" },
+                'e',
+                new List<int> {0, 1}
+            };
+            yield return new object[]
+            {
+                new string[] { "abc","bcd","aaaa","cbc" },
+                'a',
+                new List<int> {0, 2}
+            };
+            yield return new object[]
+            {
+                new string[] { "abc","bcd","aaaa","cbc" },
+                'z',
+                new List<int>()
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/2942_FindWordsContainingCharacter.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy
+{
+    public static class FindWordsContainingCharacter
+    {
+        public static IList<int> FindWordsContaining(string[] words, char x)
+        {
+            var result = new List<int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains(x))
+                    result.Add(i);
+            }
+
+            return result;
+        }
+    }
+}

6e7b198e1a29be6570fa53355973a3ad0df293d0
defqd <DefQD@yandex.ru>
2024-02-15 04:42:23 +0300

    1637. Widest Vertical Area Between Two Points Containing No Points

---
+++ LeetCode/LeetCode.Tests/Easy/1637_WidestVerticalAreaBetweenTwoPointsContainingNoPointsTests.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class WidestVerticalAreaBetweenTwoPointsContainingNoPointsTests
+    {
+        [TestCaseSource(typeof(WidestVerticalAreaBetweenTwoPointsContainingNoPointsTestSource))]
+        public void MaxWidthOfVerticalAreaTest(int[][] points, int expected)
+        {
+            var result = WidestVerticalAreaBetweenTwoPointsContainingNoPoints.MaxWidthOfVerticalArea(points);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+    class WidestVerticalAreaBetweenTwoPointsContainingNoPointsTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[][]
+                {
+                    new int[] { 8, 7 },
+                    new int[] { 9, 9 },
+                    new int[] { 7, 4 },
+                    new int[] { 9, 7 }
+                },
+                1
+            };
+            yield return new object[]
+            {
+                new int[][]
+                {
+                    new int[] { 3, 1 },
+                    new int[] { 9, 0 },
+                    new int[] { 1, 0 },
+                    new int[] { 1, 4 },
+                    new int[] { 5, 3 },
+                    new int[] { 8, 8 }
+                },
+                3
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/1637_WidestVerticalAreaBetweenTwoPointsContainingNoPoints.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    public static class WidestVerticalAreaBetweenTwoPointsContainingNoPoints
+    {
+        public static int MaxWidthOfVerticalArea(int[][] points)
+        {
+            Array.Sort(points, (a, b) => a[0].CompareTo(b[0]));
+
+            int result = 0;
+
+            for (int i = 1; i < points.Length; i++)
+            {
+                int width = points[i][0] - points[i - 1][0];
+                result = Math.Max(result, width);
+            }
+
+            return result;
+        }
+    }
+}

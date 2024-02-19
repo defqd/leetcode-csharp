8c32469d298476919af38e6e31f207b7e8298c64
defqd <DefQD@yandex.ru>
2024-02-19 04:32:08 +0300

    2798. Number of Employees Who Met the Target

---
+++ LeetCode/LeetCode.Tests/Easy/2798_NumberOfEmployeesWhoMetTheTargetTests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    internal class NumberOfEmployeesWhoMetTheTargetTests
+    {
+        [TestCaseSource(typeof(NumberOfEmployeesWhoMetTheTargetTestSource))]
+        public void NumberOfEmployeesWhoMetTargetTest(int[] hours, int target, int expected)
+        {
+            var result = NumberOfEmployeesWhoMetTheTarget.NumberOfEmployeesWhoMetTarget(hours, target);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class NumberOfEmployeesWhoMetTheTargetTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { 0, 1, 2, 3, 4 },
+                2,
+                3
+            };
+            yield return new object[]
+            {
+                new int[] { 5, 1, 4, 2, 2 },
+                6,
+                0
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/2798_NumberOfEmployeesWhoMetTheTarget.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    public static class NumberOfEmployeesWhoMetTheTarget
+    {
+        public static int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            //int count = 0;
+
+            //foreach (int hour in hours)
+            //{
+            //    if (hour >= target)
+            //        count++;
+            //}
+
+            //return count;
+
+            return hours.Where(hour => hour >= target).Count();
+        }
+    }
+}

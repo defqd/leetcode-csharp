b6b7faa59523a2b6c8568ea77bf510bda3ed283d
defqd <DefQD@yandex.ru>
2024-02-19 04:47:48 +0300

    1431. Kids With the Greatest Number of Candies

---
+++ LeetCode/LeetCode.Tests/Easy/1431_KidsWithTheGreatestNumberOfCandiesTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class KidsWithTheGreatestNumberOfCandiesTests
+    {
+        [TestCaseSource(typeof(KidsWithTheGreatestNumberOfCandiesTestSource))]
+        public void KidsWithCandiesTest(int[] candies, int extraCandies, List<bool> expected)
+        {
+            var result = KidsWithTheGreatestNumberOfCandies.KidsWithCandies(candies, extraCandies);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class KidsWithTheGreatestNumberOfCandiesTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { 2, 3, 5, 1, 3 },
+                3,
+                new List<bool> { true, true, true, false, true }
+            };
+            yield return new object[]
+            {
+                new int[] { 4, 2, 1, 1, 2 },
+                1,
+                new List<bool> { true, false, false, false, false }
+            };
+            yield return new object[]
+            {
+                new int[] { 12, 1, 12 },
+                10,
+                new List<bool> { true, false, true }
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/1431_KidsWithTheGreatestNumberOfCandies.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    public static class KidsWithTheGreatestNumberOfCandies
+    {
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            var result = new List<bool>();
+            var max = candies.Max();
+
+            foreach (int candy in candies)
+            {
+                if (candy + extraCandies >= max)
+                    result.Add(true);
+                else
+                    result.Add(false);
+            }
+
+            return result;
+        }
+    }
+}

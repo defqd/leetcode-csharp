b3865b58653563b9b31dc2732eec4f05e174cfe5
defqd <DefQD@yandex.ru>
2024-02-22 05:21:53 +0300

    2824. Count Pairs Whose Sum is Less than Target

---
+++ LeetCode/LeetCode.Tests/Easy/2824_CountPairsWhoseSumIsLessThanTargetTests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class CountPairsWhoseSumIsLessThanTargetTests
+    {
+        [TestCaseSource(typeof(CountPairsWhoseSumIsLessThanTargetTestSource))]
+        public void CountPairsTest(IList<int> nums, int target, int expected)
+        {
+            var result = CountPairsWhoseSumIsLessThanTarget.CountPairs(nums, target);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class CountPairsWhoseSumIsLessThanTargetTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { -1, 1, 2, 3, 1 },
+                2,
+                3
+            };
+            yield return new object[]
+            {
+                new int[] { -6, 2, 5, -2, -7, -1, 3 },
+                -2,
+                10
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/2824_CountPairsWhoseSumIsLessThanTarget.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Easy
+{
+    public static class CountPairsWhoseSumIsLessThanTarget
+    {
+        public static int CountPairs(IList<int> nums, int target)
+        {
+            //TwoPointers
+            nums = nums.OrderBy(x => x).ToList();
+
+            int count = 0;
+            int left = 0;
+            int right = nums.Count - 1;
+
+            while (left < right)
+            {
+                if (nums[left] + nums[right] < target)
+                {
+                    count += right - left;
+                    left++;
+                }
+                else
+                    right--;
+            }
+
+            return count;
+
+            //int count = 0;
+
+            //for (int i = 0; i < nums.Count; i++)
+            //{
+            //    for (int j = i + 1; j < nums.Count; j++)
+            //    {
+            //        if (nums[i] + nums[j] < target)
+            //            count++;
+            //    }
+            //}
+
+            //return count;
+
+
+            //return nums.Select((n, i) => nums.Skip(i + 1).Count(p => n + p < target))
+            //       .Sum();
+        }
+    }
+}

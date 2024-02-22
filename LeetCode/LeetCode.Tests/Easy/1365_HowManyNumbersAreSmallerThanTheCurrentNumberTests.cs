297d984979b8ea33f2c20a144a9b93360e4a3fda
defqd <DefQD@yandex.ru>
2024-02-22 06:18:33 +0300

    1365. How Many Numbers Are Smaller Than the Current Number

---
+++ LeetCode/LeetCode.Tests/Easy/1365_HowManyNumbersAreSmallerThanTheCurrentNumberTests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class HowManyNumbersAreSmallerThanTheCurrentNumberTests
+    {
+        [TestCaseSource(typeof(HowManyNumbersAreSmallerThanTheCurrentNumberTestSource))]
+        public void SmallerNumbersThanCurrentTest(int[] nums, int[] expected)
+        {
+            var result = HowManyNumbersAreSmallerThanTheCurrentNumber.SmallerNumbersThanCurrent(nums);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class HowManyNumbersAreSmallerThanTheCurrentNumberTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { 8, 1, 2, 2, 3 },
+                new int[] { 4, 0, 1, 1, 3 }
+            };
+            yield return new object[]
+            {
+                new int[] { 6, 5, 4, 8 },
+                new int[] { 2, 1, 0, 3 }
+            };
+            yield return new object[]
+            {
+                new int[] { 7, 7, 7, 7 },
+                new int[] { 0, 0, 0, 0 }
+            };
+
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/1365_HowManyNumbersAreSmallerThanTheCurrentNumber.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace LeetCode.Easy
+{
+    public static class HowManyNumbersAreSmallerThanTheCurrentNumber
+    {
+        public static int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            List<int> numsSorted = new List<int>(nums);
+
+            numsSorted.Sort();
+
+            int[] result = new int[nums.Length];
+
+            for (int i = 0; i < numsSorted.Count; i++)
+            {
+                result[i] = numsSorted.IndexOf(nums[i]);
+            }
+
+            return result;
+
+
+            //var dic = new Dictionary<int, int>();
+            //int[] copy = (int[])nums.Clone();
+
+            //Array.Sort(copy);
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (!dic.ContainsKey(copy[i]))
+            //    {
+            //        dic.Add(copy[i], i);
+            //    }
+            //}
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    copy[i] = dic[nums[i]];
+            //}
+
+            //return copy;
+        }
+    }
+}

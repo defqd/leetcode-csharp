e6b44e25ca6dc185924a3c6de60734210e23e3e6
defqd <DefQD@yandex.ru>
2024-02-23 07:57:11 +0300

    2114. Maximum Number of Words Found in Sentences

---
+++ LeetCode/LeetCode.Tests/Easy/2114_MaximumNumberOfWordsFoundInSentencesTests.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class MaximumNumberOfWordsFoundInSentencesTests
+    {
+        [TestCaseSource(typeof(MaximumNumberOfWordsFoundInSentencesTestSource))]
+        public void MostWordsFoundTest(string[] sentences, int expected)
+        {
+            var result = MaximumNumberOfWordsFoundInSentences.MostWordsFound(sentences);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class MaximumNumberOfWordsFoundInSentencesTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new string[] { "alice and bob love leetcode", "i think so too", "this is great thanks very much" },
+                6
+            };
+            yield return new object[]
+            {
+                new string[] { "please wait", "continue to fight", "continue to win" },
+                3
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/2114_MaximumNumberOfWordsFoundInSentences.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    public static class MaximumNumberOfWordsFoundInSentences
+    {
+        public static int MostWordsFound(string[] sentences)
+        {
+            int max = 0;
+
+            foreach (string sentence in sentences)
+            {
+                var temp = sentence.Split();
+                var length = temp.Length;
+
+                if (length > max)
+                    max = length;
+            }
+
+            return max;
+        }
+    }
+}

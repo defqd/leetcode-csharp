17cbcc9c7df36263010c46202aea5d281d2df1f5
defqd <DefQD@yandex.ru>
2024-02-27 06:55:25 +0300

    2859. Sum of Values at Indices With K Set Bits

---
+++ LeetCode/LeetCode.Tests/Easy/2859_SumOfValuesAtIndicesWithKSetBitsTests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class SumOfValuesAtIndicesWithKSetBitsTests
+    {
+        [TestCaseSource(typeof(SumOfValuesAtIndicesWithKSetBitsTestSource))]
+        public void SumIndicesWithKSetBitsTest(IList<int> nums, int k, int expected)
+        {
+            var result = SumOfValuesAtIndicesWithKSetBits.SumIndicesWithKSetBits(nums, k);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+
+    class SumOfValuesAtIndicesWithKSetBitsTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new List<int> { 5, 10, 1, 5, 2 },
+                1,
+                13
+            };
+            yield return new object[]
+            {
+                new List<int> { 4, 3, 2, 1 },
+                2,
+                1
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/2859_SumOfValuesAtIndicesWithKSetBits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace LeetCode.Easy
+{
+    public static class SumOfValuesAtIndicesWithKSetBits
+    {
+        public static int SumIndicesWithKSetBits(IList<int> nums, int k)
+        {
+            int result = 0;
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (BitOperations.PopCount((uint)i) == k)
+                {
+                    result += nums[i];
+                }
+            }
+
+            return result;
+
+            //return nums.Where((t, i) => CountOnes(i) == k).Sum();
+        }
+
+        //private static int CountOnes(int num)
+        //{
+        //    int count = 0;
+        //    while (num != 0)
+        //    {
+        //        if ((num & 1) == 1)
+        //            ++count;
+        //        num >>= 1;
+        //    }
+        //    return count;
+        //}
+    }
+}

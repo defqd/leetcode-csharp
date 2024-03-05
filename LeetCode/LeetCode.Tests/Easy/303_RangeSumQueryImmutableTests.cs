fa1cf8184d6ab54d8a373d93e503a98990e461d1
defqd <DefQD@yandex.ru>
2024-03-05 06:08:41 +0300

    303. Range Sum Query - Immutable

---
+++ LeetCode/LeetCode.Tests/Easy/303_RangeSumQueryImmutableTests.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class RangeSumQueryImmutableTests
+    {
+        [TestCaseSource(typeof(RangeSumQueryImmutableTestSource))]
+        public void SumRangeTest(int[] nums, int left, int right, int expected)
+        {
+            var range = new RangeSumQueryImmutable(nums);
+
+            var result = range.SumRange(left, right);
+
+            Assert.That(result, Is.EqualTo(expected));
+
+        }
+    }
+
+    class RangeSumQueryImmutableTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[] { -2, 0, 3, -5, 2, -1 },
+                0, 
+                2,
+                1
+            };
+            yield return new object[]
+            {
+                new int[] { -2, 0, 3, -5, 2, -1 },
+                2,
+                5,
+                -1
+            };
+            yield return new object[]
+            {
+                new int[] { -2, 0, 3, -5, 2, -1 },
+                0,
+                5,
+                -3
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/303_RangeSumQueryImmutable.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Easy
+{
+    public class RangeSumQueryImmutable
+    {
+        private readonly int[] _nums;
+        public RangeSumQueryImmutable(int[] nums)
+        {
+            _nums = nums;
+
+            //_nums = new int[nums.Length + 1];
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    _nums[i + 1] = _nums[i] + nums[i];
+            //}
+        }
+
+        public int SumRange(int left, int right)
+        {
+            int result = 0;
+
+            for (int i = left; i <= right; i++)
+                result += _nums[i];
+
+            return result;
+
+            //return _nums[right + 1] - _nums[left];
+        }
+    }
+}

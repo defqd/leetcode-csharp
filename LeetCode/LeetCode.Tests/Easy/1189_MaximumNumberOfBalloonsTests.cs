54ef505f0b07ad914f7deeb87ae5fc61aaa00de3
defqd <DefQD@yandex.ru>
2024-03-09 06:09:13 +0300

    1189. Maximum Number of Balloons

---
+++ LeetCode/LeetCode.Tests/Easy/1189_MaximumNumberOfBalloonsTests.cs
@@ -0,0 +1,48 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class MaximumNumberOfBalloonsTests
+    {
+        [TestCaseSource(typeof(MaximumNumberOfBalloonsTestSource))]
+        public void MaxNumberOfBalloonsTest(string text, int expected)
+        {
+            var result = MaximumNumberOfBalloons.MaxNumberOfBalloons(text);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+
+    class MaximumNumberOfBalloonsTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                "nlaebolko",
+                1
+            };            
+            yield return new object[]
+            {
+                "loonbalxballpoon",
+                2
+            };            
+            yield return new object[]
+            {
+                "leetcode",
+                0
+            };            
+            yield return new object[]
+            {
+                "lloo",
+                0
+            };
+            yield return new object[]
+            {
+                "balons",
+                0
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/1189_MaximumNumberOfBalloons.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode.Easy
+{
+    public static class MaximumNumberOfBalloons
+    {
+        public static int MaxNumberOfBalloons(string text)
+        {
+            var result = text.Length;
+
+            var count = new Dictionary<char, int>()
+            {
+                {'b', 0 },
+                {'a', 0 },
+                {'l', 0 },
+                {'o', 0 },
+                {'n', 0 },
+            };
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (count.ContainsKey(text[i]))
+                    count[text[i]]++;
+            }
+
+            count['l'] /= 2;
+            count['o'] /= 2;
+
+            foreach (var c in count.Values)
+                result = Math.Min(result, c);
+
+            return result;
+
+            //var result = text.Length;
+
+            //var balloon = new Dictionary<char, int>()
+            //{
+            //    {'b', 1 },
+            //    {'a', 1 },
+            //    {'l', 2 },
+            //    {'o', 2 },
+            //    {'n', 1 },
+            //};
+
+            //var count = new Dictionary<char, int>();
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    if (count.ContainsKey(text[i]))
+            //        count[text[i]]++;
+            //    else
+            //        count[text[i]] = 1;
+            //}
+
+            //foreach (char c in balloon.Keys)
+            //{
+            //    if (count.ContainsKey(c))
+            //        result = Math.Min(result, count[c] / balloon[c]);
+            //    else
+            //        return 0;
+            //}
+
+            //return result;
+        }
+    }
+}

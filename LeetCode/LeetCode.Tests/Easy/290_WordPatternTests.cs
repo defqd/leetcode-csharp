4ef1d6e4c33191a2c35495f040edaf69ef4f0cf1
defqd <DefQD@yandex.ru>
2024-03-18 07:28:43 +0300

    290. Word Pattern

---
+++ LeetCode/LeetCode.Tests/Easy/290_WordPatternTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Easy;
+using System.Collections;
+
+namespace LeetCode.Tests.Easy
+{
+    public class WordPatternTests
+    {
+        [TestCaseSource(typeof(WordPatternTestSource))]
+        public void WordPatternTest(string pattern, string s, bool expected)
+        {
+            var result = WordPattern.WordPatternMethod(pattern, s);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+
+    class WordPatternTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                "abba",
+                "dog cat cat dog",
+                true
+            };
+            yield return new object[]
+            {
+                "abba",
+                "dog cat cat fish",
+                false
+            };
+            yield return new object[]
+            {
+                "aaaa",
+                "dog cat cat dog",
+                false
+            };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/290_WordPattern.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Easy
+{
+    public static class WordPattern
+    {
+        public static bool WordPatternMethod(string pattern, string s)
+        {
+            var words = s.Split(' ');
+            var charToWord = new Dictionary<char, string>();
+            var wordToChar = new Dictionary<string, char>();
+
+            if (pattern.Length != words.Length)
+                return false;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                charToWord.TryAdd(pattern[i], words[i]);
+                wordToChar.TryAdd(words[i], pattern[i]);
+
+                if (charToWord[pattern[i]] != words[i])
+                    return false;
+                if (wordToChar[words[i]] != pattern[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
